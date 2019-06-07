a7203b9c0de2031ab3508392585fcd9f27f577b9
Giorgi Tamarashvili <46628664+giorgi01@users.noreply.github.com>
2019-06-07 20:46:55 +0400

    Create app.py

---
+++ app/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+import sqlite3
+
+
+app = Flask('blog-app', template_folder='templates')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/', methods=['POST'])
+def index_post():
+    conn = sqlite3.connect('blogs.db')
+    c = conn.cursor()
+    c.execute("""CREATE TABLE bloggers (first text, last text, post text)""")
+    firstname_text = request.form['firstname']
+    lastname_text = request.form['lastname']
+    post_text = request.form['post_text']
+    params = (firstname_text, lastname_text, post_text)
+    c.execute(f"INSERT INTO bloggers VALUES (?, ?, ?)", params)
+    conn.commit()
+    conn.close()
+    return 'Your post has been saved successfully'
+
+
+app.run()
